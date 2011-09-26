66d09f41ef9d5aa5b046f67d79fac9ebf9602b79
Mikkel Munch Mortensen <3xm@detfalskested.dk>
2011-09-26 15:45:54 +0200

    Plugins are told about (all) new/gone devices. And Notifier, a sample plugin.

---
+++ plugins/notify.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pynotify, sys
+
+if not pynotify.init('icon-summary'):
+	sys.exit(1)
+
+class Notifier:
+	# Make an onscreen notification
+	def notify(self, title, in_range):
+		n = pynotify.Notification(title, '', 'file:///usr/share/icons/ubuntu-mono-dark/status/24/bluetooth-active.svg' if in_range else 'file:///usr/share/icons/ubuntu-mono-dark/status/24/bluetooth-disabled.svg')
+		n.show()
+
+	def in_range(self, id, name):
+		self.notify('%s (%s) in range' % (name, id,), True)
+	
+	def out_of_range(self, id, name):
+		self.notify('%s (%s) out of range' % (name, id,), False)
